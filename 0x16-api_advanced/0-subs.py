3ef941d186130e948ef4ee7945363941950e5d0b
therealmahmoud~ <moha91875460@gmail.com>
2024-05-07 21:55:38 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,8 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     headers = {'User-Agent': 'MyAPI/0.0.4'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
     data = response.json()
-    subscribers = data['data']['subscribers']
-    return subscribers
+    if response.status_code == 200:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    return 0
