c2c89db8b491bafd919f884c63cac22355895645
therealmahmoud~ <moha91875460@gmail.com>
2024-05-07 22:15:19 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
-        'User-Agent': 'MyBot/0.1 (by /u/mahmoud)'
+        'User-Agent': 'MyBot/0.10.6 (by /u/mahmoud)'
         }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
