1aee55f27ed4127d2ff4b018bf06e983f11eefdd
therealmahmoud~ <moha91875460@gmail.com>
2024-05-07 22:06:17 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers (not active users,
     total subscribers) for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     headers = {'User-Agent': 'Mahmoud/0.1'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 404:
         return 0
