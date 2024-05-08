9ab85427d8cfd14a2deab2425f8edc087ba32ef1
therealmahmoud~ <moha91875460@gmail.com>
2024-05-09 02:54:50 +0300

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fixing internal server error 500.
-exec { 'Fix apache':
-  command  => 'sudo sed -i "s/.phpp/.php" /var/www/html/wp-settings.php',
-  provider =>  shell,
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
